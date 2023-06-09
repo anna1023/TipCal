7416681fcaa9e8aa7d7398f81dc418a6a5124285
test <annac101@nycstudents.net>
2023-06-09 15:34:50 -0400

    5

--- src/MainGUIWindow.java
+++ src/MainGUIWindow.java
@@ -1,8 +1,6 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyListener;
-import java.awt.event.KeyEvent;
 
 public class MainGUIWindow extends JFrame implements ActionListener{
     private JPanel mainPanel;
@@ -11,10 +9,10 @@ public class MainGUIWindow extends JFrame implements ActionListener{
     private JTextArea textArea3;
     private JTextArea textArea4;
     private JTextArea textArea5;
-    private JButton as;
-    private JButton de;
-    private JButton cend;
-    private JButton sned;
+    private JButton tipup;
+    private JButton tipdown;
+    private JButton numup;
+    private JButton numdown;
     private JButton calculateButton;
     private JLabel Bill;
     private JLabel tip;
@@ -33,11 +31,13 @@ public class MainGUIWindow extends JFrame implements ActionListener{
         setSize(700,400);
         setLocation(450,100);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        as.addActionListener(this);
-        cend.addActionListener(this);
-        de.addActionListener(this);
+        tipup.addActionListener(this);
+        numup.addActionListener(this);
+        tipdown.addActionListener(this);
         calculateButton.addActionListener(this);
-        sned.addActionListener(this);
+        numdown.addActionListener(this);
+        textArea4.setText("0");
+        textArea5.setText("0");
         this.setVisible(true);
     }
 
@@ -47,25 +47,25 @@ public class MainGUIWindow extends JFrame implements ActionListener{
             JButton button = (JButton) source;
             String text = button.getText();
 
-            if (button == as){
-                double tip = Double.parseDouble(textArea4.getText());
+            if (button == tipup){
+                int tip = Integer.parseInt(textArea4.getText());
                 tip++;
                 textArea4.setText("" + tip);
             }
-            if(button == de){
-                double tip = Double.parseDouble(textArea4.getText());
+            if(button == tipdown){
+                int tip = Integer.parseInt(textArea4.getText());
                 tip--;
                 textArea4.setText("" + tip);
             }
-            if(button == cend){
-                double num = Double.parseDouble(textArea5.getText());
+            if(button == numup){
+                int num = Integer.parseInt(textArea5.getText());
                 num++;
                 textArea5.setText("" + num);
             }
-            if(button == sned){
-                double tip = Double.parseDouble(textArea5.getText());
-                tip--;
-                textArea5.setText("" + tip);
+            if(button == numdown){
+                int num = Integer.parseInt(textArea5.getText());
+                num--;
+                textArea5.setText("" + num);
             }
             if(button == calculateButton){
                 TipCalculator calc = new TipCalculator(Double.parseDouble(textArea2.getText()),Integer.parseInt(textArea4.getText()),Integer.parseInt(textArea5.getText()));
